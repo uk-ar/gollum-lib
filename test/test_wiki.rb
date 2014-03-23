3148e4fcf9a454fe8451b09dc6f55547b335748f
uk-ar <yuuki.ari@gmail.com>
2014-03-24 06:32:19 +0900

    0 failures, 62 errors in rake test TEST=test/test_wiki.rb

--- test/test_wiki.rb
+++ test/test_wiki.rb
@@ -95,8 +95,6 @@ context "Wiki" do
     assert_no_match regex, diff
   end
 
-  #skip
-
   test "gets scoped page from specified directory" do
     @path = cloned_testpath('examples/lotr.git')
     begin
